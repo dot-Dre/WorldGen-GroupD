0ae8df7eb76f74c0de1376bbd15caca303359c32
Andre Lepardo <lepardandr@ecs.vuw.ac.nz>
2023-07-31 15:45:33 +1200

    Test revision of bat file

--- run.bat
+++ run.bat
@@ -1,11 +1,24 @@
 @echo off
 
-rem Start mvn spring-boot:run in the background
-start /B mvn spring-boot:run
+rem Find the process ID (PID) of the processes using ports 3000 and 8080
+for /f "tokens=2 delims= " %%a in ('netstat -ano ^| find "LISTENING" ^| find ":3000 "') do set "REACT_PORT=%%a"
+for /f "tokens=2 delims= " %%a in ('netstat -ano ^| find "LISTENING" ^| find ":8080 "') do set "SPRING_PORT=%%a"
 
-rem Change the directory to src/main/ui and run npm start
-cd src/main/ui
-start /B npm install
-npm start
+rem Terminate the processes using the identified ports (SIGINT equivalent)
+taskkill /PID %REACT_PORT% /F
+taskkill /PID %SPRING_PORT% /F
 
-rem The batch file continues executing here...
+rem Change directory to src/main/ui
+cd src\main\ui
+
+rem Install npm dependencies
+npm install
+
+rem Start the npm application in the background
+start npm start
+
+rem Change directory back to the root directory of the project
+cd ..\..\..
+
+rem Run Maven spring-boot:run command
+mvn spring-boot:run
