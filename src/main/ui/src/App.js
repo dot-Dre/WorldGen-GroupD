f40496de20a749905443d0b172bbe0314374cbd4
Andre Lepardo <lepardandr@ecs.vuw.ac.nz>
2023-08-14 20:51:17 +1200

    Built initial Web Application structure

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -1,24 +1,21 @@
-import React, { useState } from 'react';
-import './App.css';
-import { getImageFromServer } from './components/button';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Generate from './pages/Generate';
+import DMView from './pages/DMView';
+import PlayerView from '/pages/PlayerView';
 
 function App() {
-
-  const [imageSrc, setImageSrc] = useState(null);
-
-  const handleButtonClick = () => {
-    getImageFromServer()
-      .then(imageUrl => setImageSrc(imageUrl))
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  };
-
   return (
-    <div className="App">
-      <button onClick={handleButtonClick}>Get Image from Backend</button>
-      {imageSrc && <img src={imageSrc} alt="Received from server" />}
-    </div>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path = "/" element={<Home />} />
+        <Route path = "/Generate" element={<Generate />} />
+        <Route path = "/DMView" element={<DMView />} />
+        <Route path = "/PlayerView" element={<PlayerView />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/main/ui/src/pages/Generate.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+function Generate() {
+    return <h1>Generate</h1>
+}
+
+export default Generate;
