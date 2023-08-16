b2ee12db4fb928e09f6733a94ff0f62f39110d31
Andre Lepardo <lepardandr@ecs.vuw.ac.nz>
2023-08-17 09:57:01 +1200

    Added Theme.js

---
+++ src/main/ui/src/Theme.js
@@ -0,0 +1,27 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+/**
+ * This is a theme constant which should be wrapped around all styled MUI components
+ * usage:
+ *  import theme from 'path/to/here'
+ *  <ThemeProvider theme={theme}>
+ *      * Other Comps *
+ *  </ThemeProvider>
+ */
+export const theme = createTheme({
+    // Add more if needed
+    palette: {
+        primary: {
+            main: "#7711C6"
+            // light: will be calculated from palette.primary.main,
+            // dark: will be calculated from palette.primary.main,
+            // contrastText: will be calculated to contrast with palette.primary.main
+        },
+        secondary: {
+            main: "#92DF9B"
+        },
+        tertiary: {
+            main: "#A40D08"
+        }
+    },
+});
