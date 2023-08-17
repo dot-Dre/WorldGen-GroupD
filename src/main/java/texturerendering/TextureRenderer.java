fc9ea08cfb863d2dcda88b3df9e62cfffe663226
mamoonil <mamoonil@myvuw.ac.nz>
2023-08-17 16:52:40 +1200

    Add initial version of TextureRenderer class
    - Introduced TextureRenderer class with placeholders for methods and fields
    - Pending implementation for constructor, classifyRoom(), and renderTexturesAndProps()

---
+++ src/main/java/texturerendering/TextureRenderer.java
@@ -0,0 +1,50 @@
+package texturerendering;
+
+import java.util.Map;
+
+public class TextureRenderer {
+
+    private Map<String, Texture> textureMap;
+    private Map<String, Prop> propMap;
+
+    /**
+     * Constructor for TextureRenderer.
+     * TO BE IMPLEMENTED
+     * Initializes textureMap and propMap.
+     */
+    public TextureRenderer() {
+        //initialize textureMap and propMap
+        //placeholder
+    }
+
+    /**
+     * Classifies a room based on its attributes. such as size, position, and orientation.
+     * TO BE IMPLEMENTED
+     *
+     * @param room The room to classify.
+     * @return The room classification.
+     */
+    public void classifyRoom() {
+        // Determine the room classification based on its attributes
+        // Return the room classification
+    }
+
+    /**
+     * Renders the textures and props onto the map.
+     * TO BE IMPLEMENTED
+     * Iterates through each room in the dungeon.
+     * Classifies the room.
+     * Gets the appropriate textures and props based on the room classification.
+     * Renders the room's base texture and props onto the map.
+     * Overlays them based on room position, size, and orientation.
+     */
+    public void renderTexturesAndProps() {
+        // Iterate through each room in the dungeon
+        //for (Room room : dungeon.getRooms()) {
+        // Classify the room
+        // Get the appropriate textures and props based on the room classification
+        // Render the room's base texture and props onto the map
+        // Overlay them based on room position, size, and orientation
+        //  }
+    }
+}
