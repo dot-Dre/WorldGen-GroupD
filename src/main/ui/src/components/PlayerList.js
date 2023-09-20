cad6a7636c21bf50d3b1c53538798f93bed933d3
mckeeffinn <finnmckeefry99@gmail.com>
2023-09-21 03:03:52 +1200

    Implemented Initial PlayerView Page Adjusted PlayerList.js and PlayerList.css to include more elements for improved reusability in future Additionally, created Notes.js to make a persistent text box element

--- src/main/ui/src/components/PlayerList.js
+++ src/main/ui/src/components/PlayerList.js
@@ -7,10 +7,12 @@ import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
+import { Typography } from "@mui/material";
 import Button from "@mui/material/Button";
+import Notes from "../components/Notes";
 import "./PlayerList.css";
 
-const PlayerList = ({ initialData, height, sx, }) => {
+const PlayerList = ({ initialData, height, sx, gameID, isDMView }) => {
   const [data, setData] = useState(initialData);
   const [openDialog, setOpenDialog] = useState(false);
   const [selectedPlayer, setSelectedPlayer] = useState(null);
@@ -45,48 +47,115 @@ const PlayerList = ({ initialData, height, sx, }) => {
     backgroundColor: "#FFFFFF",
     borderRadius: "4px",
     ...(sx || {}),
+    marginLeft: "calc((100% - 135%) / 2)",
+    marginRight: "calc((100% - 135%) / 2)",
+    width: "auto",
+    maxWidth: "150%",
+  };
+
+  const playersTypographyStyle = {
+    textAlign: "left",
+    marginLeft: "calc((100% - 135%) / 2)",
+    padding: "3vh 0px 1.2vh 0px",
+  };
+
+  const gameIDTypographyStyle = {
+    textTransform: "uppercase",
+    fontFamily: "fantasy",
+    textAlign: "left",
+    marginLeft: -30,
+    marginTop: -30,
+  };
+
+  const gameIDNumberTypographyStyle = {
+    textAlign: "left",
+    marginLeft: -30,
+    marginTop: -10,
   };
 
   const dialogStyle = {
     innerBackgroundColor: "#FFFFFF",
     outerBackgroundColor: "#339e7a",
-  }
+  };
 
   return (
     <div>
+      <Typography variant="h5" gutterBottom style={gameIDTypographyStyle}>
+        Game ID:
+      </Typography>
+      <Typography style={gameIDNumberTypographyStyle}>{gameID}</Typography>
+      <Notes />
+      <Typography variant="h6" style={playersTypographyStyle}>
+        {isDMView ? "Players" : "Party"}
+      </Typography>
       <List style={listStyle}>
         {data.map((item, index) => (
           <ListItem key={index} onClick={() => handleClickOpen(item)}>
-          <ListItemButton color="primary" style={{ fontFamily: "monospace", borderRadius: "4px" }}>
-            <ListItemText
-              primary={<span style={{ fontFamily: 'monospace', fontSize: '10px' }}>{item.name}</span>}
-              secondary={
-                <React.Fragment>
-                  <span style={{ fontFamily: 'monospace', fontSize: '9px' }}>Role: {item.role}</span>
-                  <br />
-                  <span style={{ fontFamily: 'monospace', fontSize: '9px' }}>Status: {item.status}</span>
-                </React.Fragment>
-              }
-            />
-            <div style={{ marginLeft: "auto", fontFamily: "monospace", fontSize: '9px' }}>
-              X: {item.x}, Y: {item.y}
-            </div>
-          </ListItemButton>
-        </ListItem>
-        
+            <ListItemButton
+              color="primary"
+              style={{ fontFamily: "monospace", borderRadius: "4px" }}
+            >
+              <ListItemText
+                primary={
+                  <span style={{ fontFamily: "monospace", fontSize: "1.2vw" }}>
+                    {item.name}
+                  </span>
+                }
+                secondary={
+                  <React.Fragment>
+                    <span
+                      style={{ fontFamily: "monospace", fontSize: "1.2vw" }}
+                    >
+                      Role: {item.role}
+                    </span>
+                    <br />
+                    <span
+                      style={{ fontFamily: "monospace", fontSize: "1.2vw" }}
+                    >
+                      Status: {item.status}
+                    </span>
+                  </React.Fragment>
+                }
+              />
+              <div
+                style={{
+                  marginLeft: "auto",
+                  fontFamily: "monospace",
+                  fontSize: "1.2vw",
+                  marginTop: "-5vh",
+                }}
+              >
+                <div>X: {item.x}</div>
+                <div>Y: {item.y}</div>
+              </div>
+            </ListItemButton>
+          </ListItem>
         ))}
       </List>
 
-      <Dialog open={openDialog} onClose={handleClose} style={{ fontFamily: 'monospace' }}>
-        <DialogTitle style={{backgroundColor: dialogStyle.outerBackgroundColor, fontFamily: 'monospace'}}>Change Status</DialogTitle>
-        <DialogContent style={{backgroundColor: dialogStyle.innerBackgroundColor}}>
+      <Dialog
+        open={openDialog}
+        onClose={handleClose}
+        style={{ fontFamily: "monospace" }}
+      >
+        <DialogTitle
+          style={{
+            backgroundColor: dialogStyle.outerBackgroundColor,
+            fontFamily: "monospace",
+          }}
+        >
+          Change Status
+        </DialogTitle>
+        <DialogContent
+          style={{ backgroundColor: dialogStyle.innerBackgroundColor }}
+        >
           {selectedPlayer && (
             <div>
               <p>Player: {selectedPlayer.name}</p>
               <p>Current Status: {selectedPlayer.status}</p>
               <p>Change Status:</p>
               <input
-                style={{fontFamily: 'monospace'}}
+                style={{ fontFamily: "monospace" }}
                 type="text"
                 value={newStatus}
                 onChange={(e) => setNewStatus(e.target.value)}
@@ -94,8 +163,14 @@ const PlayerList = ({ initialData, height, sx, }) => {
             </div>
           )}
         </DialogContent>
-        <DialogActions style={{backgroundColor: dialogStyle.outerBackgroundColor}}>
-          <Button onClick={handleChangeStatus} color="primary" variant="outlined">
+        <DialogActions
+          style={{ backgroundColor: dialogStyle.outerBackgroundColor }}
+        >
+          <Button
+            onClick={handleChangeStatus}
+            color="primary"
+            variant="outlined"
+          >
             Save
           </Button>
         </DialogActions>
