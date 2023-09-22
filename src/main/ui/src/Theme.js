a03a316ceb38fb20b809a995bf9bec7d073f5185
Andre Lepardo <lepardandr@ecs.vuw.ac.nz>
2023-09-22 15:24:17 +1200

    Adding supporting documentation to Theme.js

--- src/main/ui/src/Theme.js
+++ src/main/ui/src/Theme.js
@@ -26,26 +26,52 @@ export const theme = createTheme({
     },
 });
 
+// USE THESE COLORS
 export const ourPalette = {
+    // Purple
     primary: "#7711C6",
+
+    // Light Green
     secondary: "#92DF9B",
+
+    // Red I think
     tertiary: "#A40D08",
+
+    // white
     white: "#FFFFFF",
+
+    // dark grey
     disabled: "#9d82ab",
+
+    // black
     black: "#000000",
+
+    // dark grey
     blank:"#1a191c",
+
+    // even darker grey
     blankDarker:"",
+
+    // light grey
     smooth:"#87868a",
+
+    // The page background gradient
     pageGradient: "linear-gradient(90deg, rgba(20,5,46,1) 21%, rgba(0,0,0,1) 50%)",
+
+    // The tab background gradient
     tabGradient: "linear-gradient(rgba(20,5,46,1) 21%, rgba(0,0,0,1) 50%)",
+
+    // the modal gradient
     modalGradient: "linear-gradient(rgba(2,0,36,1) 0%, rgba(222,207,242,1) 0%, rgba(20,14,24,1) 96%)",
 
+    // These colors are the "map size" button border colors
     buttonBorders: {
-        light: "#f2f207",
-        base: "#b7ff00",
-        dark: "#3fdb07"
+        light: "#f2f207", // s
+        base: "#b7ff00", // m
+        dark: "#3fdb07" // l
     },
 
+    // The main button background color (the dark grey)
     buttonColors: {
         primary: "#080114"
     }
