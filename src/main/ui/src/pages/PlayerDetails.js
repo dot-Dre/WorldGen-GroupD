61a8e778b72056d3a99b60777e545cab67e9d09c
Andre Lepardo <lepardandr@ecs.vuw.ac.nz>
2023-09-24 00:26:20 +1200

    Finalizing PlayerDetails styles

--- src/main/ui/src/pages/PlayerDetails.js
+++ src/main/ui/src/pages/PlayerDetails.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { SplitScreen } from "../components/SplitScreen";
 import { ThemeProvider } from "@mui/material/styles";
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, Typography } from "@mui/material";
 import theme from "../Theme";
 import "./PlayerDetails.css";
 import { useNavigate } from "react-router-dom";
@@ -11,11 +11,26 @@ import { setPlayerName } from "../slices/playerNameSlice";
 import { setPlayerRole } from "../slices/roleSlice";
 import { ourPalette } from "../Theme";
 import character from "./assets/hood.png";
-import { EnterYourNameStyles, EnterYourRoleStyles, HoodPersonStyles, InputNameTextFieldStyles, InstructionStyles, NameTextFieldStyles, StartButtonStyles, WhoAreYouStyles } from "./PlayerDetailsStyle";
+import * as GiIcons from "react-icons/gi";
+import {
+  EnterYourNameStyles,
+  EnterYourRoleStyles,
+  ErrorButtonStyles,
+  ErrorMessageStyles,
+  ErrorStyles,
+  HoodPersonStyles,
+  InputNameTextFieldStyles,
+  InstructionStyles,
+  NameTextFieldStyles,
+  SkullError,
+  StartButtonStyles,
+  WhoAreYouStyles,
+} from "./PlayerDetailsStyle";
 
 function PlayerDetails() {
   // Navigation hook
   const navigateToPlayerView = useNavigate();
+  const navigateToHome = useNavigate();
 
   // name and role variables
   const [name, setName] = useState("");
@@ -26,11 +41,14 @@ function PlayerDetails() {
   // can be uncommented to use localStorage instead.
   const dispatch = useDispatch();
   const handleAdventureStart = () => {
-    dispatch(setPlayerName(name));
-    dispatch(setPlayerRole(role));
-    // localStorage.setItem('playerName', name); localStorage.setItem('playerRole', role);
+    // dispatch(setPlayerName(name));
+    // dispatch(setPlayerRole(role));
+    localStorage.setItem("playerName", name);
+    localStorage.setItem("playerRole", role);
     navigateToPlayerView("/PlayerView");
   };
+
+  var hasGameCode = localStorage.getItem("gameCode") != null;
 
   return (
     <body
@@ -47,68 +65,74 @@ function PlayerDetails() {
         exit={{ opacity: 0 }}
       >
         <ThemeProvider theme={theme}>
-          <SplitScreen leftSpace={1} rightSpace={3}>
-            <div>
-              <h1
-                style={WhoAreYouStyles}
-              >
-                Who Are You?
-              </h1>
-              <h3
-                style={InstructionStyles}
-              >
-                Before you enter the game, inscribe your name and role below.
-              </h3>
-              <h4
-                style={EnterYourNameStyles}
-              >
-                Enter your Name
-              </h4>
-              <TextField
-                sx={NameTextFieldStyles}
-                InputProps={InputNameTextFieldStyles}
-                label="Name"
-                variant="filled"
-                color="secondary"
-                autoFocus={true}
-                fullWidth={true}
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-              <h4
-                style={EnterYourRoleStyles}
-              >
-                Declare your Role
-              </h4>
-              <TextField
-                sx={NameTextFieldStyles}
-                InputProps={InputNameTextFieldStyles}
-                label="Role"
-                variant="filled"
-                color="secondary"
-                autoFocus={true}
-                fullWidth={true}
-                value={role}
-                onChange={(e) => setRole(e.target.value)}
-              />
-              <Button
-                variant="outlined"
-                size="large"
-                onClick={handleAdventureStart}
-                disabled={isButtonDisabled}
-                sx={StartButtonStyles}
-              >
-                Start Your Adventure
-              </Button>
-            </div>
-            <div>
-              <img
-                src={character}
-                alt="crashed"
-                style={HoodPersonStyles}
-              />
-            </div>
-          </SplitScreen>
+          {hasGameCode ? (
+            <SplitScreen leftSpace={1} rightSpace={3}>
+              <div>
+                <h1 style={WhoAreYouStyles}>Who Are You?</h1>
+                <h3 style={InstructionStyles}>
+                  Before you enter the game, inscribe your name and role below.
+                </h3>
+                <h4 style={EnterYourNameStyles}>Enter your Name</h4>
+                <TextField
+                  sx={NameTextFieldStyles}
+                  InputProps={InputNameTextFieldStyles}
+                  label="Name"
+                  variant="filled"
+                  color="secondary"
+                  autoFocus={true}
+                  fullWidth={true}
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <h4 style={EnterYourRoleStyles}>Declare your Role</h4>
+                <TextField
+                  sx={NameTextFieldStyles}
+                  InputProps={InputNameTextFieldStyles}
+                  label="Role"
+                  variant="filled"
+                  color="secondary"
+                  autoFocus={true}
+                  fullWidth={true}
+                  value={role}
+                  onChange={(e) => setRole(e.target.value)}
+                />
+                <Button
+                  variant="outlined"
+                  size="large"
+                  onClick={handleAdventureStart}
+                  disabled={isButtonDisabled}
+                  sx={StartButtonStyles}
+                >
+                  Start Your Adventure
+                </Button>
+              </div>
+              <div>
+                <img src={character} alt="crashed" style={HoodPersonStyles} />
+              </div>
+            </SplitScreen>
+          ) : (
+            <>
+              <center>
+                <Typography variant="h1" style={ErrorStyles}>
+                  Gamecode not found
+                  <GiIcons.GiDeathSkull style={SkullError} />
+                </Typography>
+                <p style={ErrorMessageStyles}>
+                  You don't appear to have a gamecode entered! Re-enter your
+                  game code by clicking the button below!
+                </p>
+                <Button
+                  variant="outlined"
+                  style={ErrorButtonStyles}
+                  onClick={() => {
+                    navigateToHome("/");
+                  }}
+                >
+                  Home
+                </Button>
+              </center>
+            </>
+          )}
         </ThemeProvider>
       </motion.div>
     </body>
