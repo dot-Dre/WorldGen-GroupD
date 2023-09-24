2e7262a3fa3c79d863928e3e1269ebe60bcc2abb
rajasoor <rajasoor@myvuw.ac.nz>
2023-09-24 19:46:46 +1300

    Created HomePage popup

--- src/main/ui/src/pages/Home.js
+++ src/main/ui/src/pages/Home.js
@@ -8,7 +8,8 @@ import { ThemeProvider } from "@mui/material/styles";
 import theme from "../Theme";
 import { motion } from "framer-motion";
 import * as BiIcons from "react-icons/bi";
-import { ourPalette } from "../Theme";
+import { Divider } from "@mui/material";
+
 import {
   HomeBody,
   LogoStyles,
@@ -30,64 +31,39 @@ import "./Home.css";
 import logo from "./assets/logo.png";
 import gen from "./assets/gen.gif";
 
-/**
- * This function returns the Home page component. This page
- * serves as a bridge for users of the application to enter a running game,
- * or generate a new map and host a new game off a newly
- * generated map.
- *
- * @returns This function returns the Home page component.
- */
 export const Home = () => {
-  // Navigation hooks
   const navigateToGenerate = useNavigate();
-  const navigateToView = useNavigate();
 
-  // Hover state constants
   const [loadButtonColor, setLoadButtonColor] = useState("primary");
   const [createButtonColor, setCreateButtonColor] = useState("primary");
   const [goButtonColor, setGoButtonColor] = useState("primary");
 
-  // These will render different help texts
   const [isLoadButtonHovered, setIsLoadButtonHovered] = useState(false);
   const [isGenerateButtonHovered, setIsGenerateButtonHovered] = useState(false);
+  const [generateModalOpen, setGenerateModalOpen] = useState(false);
 
-  // Modal code
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [open, setOpen] = useState(false);
 
-  // Code to Handle file drag drop events . . . extend functionality here, currently does not save to local browser
   const [file, setFile] = useState(null);
-  const onFileChange = (file) => {
-    setFile(file);
-    // localStorage.setItem('dungeonFile', file)
-  };
+  const onFileChange = (file) => setFile(file);
 
-  // Code to Handle gamecode text field changes . . . extend functionality here
-  const [gameCode, setGameCode] = useState(""); // Initialize gameCode as an empty string
+  const [gameCode, setGameCode] = useState("");
   const [inputError, setInputError] = useState(false);
 
   const handleGameCodeChange = (event) => {
     const inputValue = event.target.value;
     setGameCode(inputValue);
-    setInputError(inputValue.length !== 9 || !/^\d+$/.test(inputValue))
+    setInputError(inputValue.length !== 9 || !/^\d+$/.test(inputValue));
   };
 
-  const gameCodeSubmit = () => {
-    localStorage.setItem("gameCode", gameCode)
-    // More logic here
-  }
+  const gameCodeSubmit = () => localStorage.setItem("gameCode", gameCode);
 
   return (
-    <body
-    style={HomeBody}
-    // className="HomeBody"
-    >
+      <body style={HomeBody}>
       <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
       >
         <ThemeProvider theme={theme}>
           <SplitScreen leftSpace={2} rightSpace={1.5}>
@@ -96,57 +72,41 @@ export const Home = () => {
               <div>
                 <h2 style={EnterIDStyles}>Enter A Game ID</h2>
                 <TextField
-                  sx={CodeFieldStyles}
-                  label="9-digit Code"
-                  variant="filled"
-                  color="secondary"
-                  autoFocus="True"
-                  value={gameCode} // Set value to gameCode
-                  onChange={handleGameCodeChange} // Add onChange handler
-                  error={inputError} // Show error state
+                    sx={CodeFieldStyles}
+                    label="9-digit Code"
+                    variant="filled"
+                    color="secondary"
+                    autoFocus="True"
+                    value={gameCode}
+                    onChange={handleGameCodeChange}
+                    error={inputError}
                 />
                 <Button
-                  sx={RocketButtonStyles}
-                  variant="outlined"
-                  size="small"
-                  color={goButtonColor}
-                  disabled={inputError}
-                  onClick={() => {
-                    if (gameCode.length !== 9 || !/^\d+$/.test(gameCode)) {
-                      setInputError(true);
-                    } else {
-                      setInputError(false);
-                      navigateToView("/PlayerDetails");
-                      gameCodeSubmit();
-                    }
-                  }}
-                  onMouseEnter={() => {
-                    setGoButtonColor("secondary");
-                  }}
-                  onMouseLeave={() => {
-                    setGoButtonColor("primary");
-                  }}
+                    sx={RocketButtonStyles}
+                    variant="outlined"
+                    size="small"
+                    color={goButtonColor}
+                    disabled={inputError}
+                    onClick={gameCodeSubmit}
                 >
                   <Typography variant="body1">
-                    {/* Go! */}
                     <BiIcons.BiSolidRocket />
                   </Typography>
                 </Button>
               </div>
+
               {inputError ? (
-                <p style={EnterHelpStyles}>
-                  Your game code isn't formatted correctly!
-                </p>
+                  <p style={EnterHelpStyles}>Your game code isn't formatted correctly!</p>
               ) : (
-                <p style={EnterHelpStyles}>e.g 143344561</p>
+                  <p style={EnterHelpStyles}>e.g 143344561</p>
               )}
-              <div>
-                <h3 style={GenerateTextStyles}>Generate A Dungeon!</h3>
-                <Button
+
+              <h3 style={GenerateTextStyles}>Generate A Dungeon!</h3>
+              <Button
                   variant="outlined"
                   size="small"
                   color={createButtonColor}
-                  onClick={() => navigateToGenerate("/Generate")}
+                  onClick={() => setGenerateModalOpen(true)}
                   onMouseEnter={() => {
                     setIsGenerateButtonHovered(true);
                     setCreateButtonColor("secondary");
@@ -156,20 +116,14 @@ export const Home = () => {
                     setCreateButtonColor("primary");
                   }}
                   style={CreateButtonStyles}
-                >
-                  <Typography
-                    variant="body1"
-                    sx={CreateButtonTextStyles}
-                  >
-                    Create Dungeon
-                  </Typography>
-                </Button>
-                <h3
-                  style={LoadTextStyles}
-                >
-                  Load A Previously Generated Dungeon!
-                </h3>
-                <Button
+              >
+                <Typography variant="body1" sx={CreateButtonTextStyles}>
+                  Create Dungeon
+                </Typography>
+              </Button>
+
+              <h3 style={LoadTextStyles}>Load A Previously Generated Dungeon!</h3>
+              <Button
                   variant="outlined"
                   color={loadButtonColor}
                   size="small"
@@ -181,46 +135,88 @@ export const Home = () => {
                     setIsLoadButtonHovered(false);
                     setLoadButtonColor("primary");
                   }}
-                  onClick={handleOpen}
+                  onClick={() => setOpen(true)}
                   style={LoadButtonStyles}
-                >
-                  <Typography
-                    variant="body1"
-                    sx={LoadButtonTextStyles}
-                  >
-                    Load Dungeon
-                  </Typography>
-                </Button>
-                <Modal
-                  open={open}
-                  onClose={handleClose}
-                  aria-labelledby="modal-modal-title"
-                  aria-describedby="modal-modal-description"
-                >
-                  <DragDrop onFileChange={(files) => onFileChange(files)} />
-                </Modal>
-              </div>
-              <div>
-                <p
-                  style={DescriptionTextStyles}
-                >
-                  {isLoadButtonHovered
-                    ? 'Rediscover history with the "Load" button. Revisit a curated Dungeons & Dragons adventure, where challenges and treasures await from previous explorers. Relive the experience, uncovering the tales etched into this pre-generated world.'
+              >
+                <Typography variant="body1" sx={LoadButtonTextStyles}>
+                  Load Dungeon
+                </Typography>
+              </Button>
+
+              <p style={DescriptionTextStyles}>
+                {isLoadButtonHovered
+                    ? 'Rediscover history with the "Load" button...'
                     : isGenerateButtonHovered
-                    ? 'Unleash terror with the "Generate" button. Dive into a chilling Dungeons & Dragons adventure where every corridor holds dread, creatures lurk in darkness, and traps test your mettle. Embark on a journey that melds horror and strategy, as you navigate a procedurally crafted nightmare.'
-                    : ""}
-                </p>
-              </div>
+                        ? 'Unleash terror with the "Generate" button...'
+                        : ""}
+              </p>
+
+              {/* Load Dungeon Modal */}
+              <Modal
+                  open={open}
+                  onClose={() => setOpen(false)}
+              >
+                <DragDrop onFileChange={(files) => onFileChange(files)} />
+              </Modal>
+
+              {/* Generate Dungeon Modal */}
+              <Modal
+                  open={generateModalOpen}
+                  onClose={() => setGenerateModalOpen(false)}
+                  aria-labelledby="dungeon-modal-title"
+                  aria-describedby="dungeon-modal-description"
+              >
+                <div
+                    style={{
+                      top: '50%',
+                      left: '50%',
+                      transform: 'translate(-50%, -50%)',
+                      position: 'absolute',
+                      background: 'white',
+                      padding: '20px',
+                      boxShadow: '2px 2px 10px rgba(0, 0, 0, 0.3)',
+                      borderRadius: '10px'
+                    }}
+                >
+                  <Button
+                      variant="outlined"
+                      style={{ marginBottom: '10px' }}
+                      onClick={() => {
+                        console.log("Quick generate logic goes here");
+                        setGenerateModalOpen(false);
+                      }}
+                  >
+                    Quick Generate
+                  </Button>
+                  <Typography variant="body2" style={{ marginBottom: '15px' }}>
+                    Generate a map with a press of a button, instantly dive into a new DnD adventure!
+                  </Typography>
+
+                  <Divider style={{ marginBottom: '15px', marginTop: '15px' }} /> {/* <-- The Divider */}
+
+                  <Button
+                      variant="outlined"
+                      style={{ marginBottom: '10px' }}
+                      onClick={() => {
+                        navigateToGenerate("/Generate");
+                        setGenerateModalOpen(false);
+                      }}
+                  >
+                    Custom Generate
+                  </Button>
+                  <Typography variant="body2">
+                    Customize your story! Select map size and dungeon theme to create a dungeon that's tailored for the story you want to tell!
+                  </Typography>
+                </div>
+              </Modal>
+              <Divider />
+
             </Container>
-            <img
-              src={gen}
-              alt="crashed"
-              style={LeftImageStyles}
-            />
+            <img src={gen} alt="crashed" style={LeftImageStyles} />
           </SplitScreen>
         </ThemeProvider>
       </motion.div>
-    </body>
+      </body>
   );
 };
 
